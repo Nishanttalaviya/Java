3b1aad963c34e425ce175aeabb0ee08847661bac
nishant talaviya <nishanttalaviya2308@gmail.com>
2024-09-24 18:25:23 +0530

    add

---
+++ ABC.java
@@ -0,0 +1,10 @@
+public class ABC{
+	public static void main(String[] args){
+	String s="say ABC ABC ABC";
+	System.out.println(s);
+	System.out.println("lastIndexOf(\B\)=");
+	System.out.println("s.lastIndexOf('B')=");
+	System.out.println("lastIndexOf("\AB\")=");
+	System.out.println("lastIndexOf("AB")="));
+	}
+}
---
+++ Arithmetic.java
@@ -0,0 +1,25 @@
+/*
+arithmetic opretion
++,-,/,*
+*/
+
+class Arithmetic
+{
+	public static void main(String args[])
+	{
+		int a=10,b=20
+		System.out.println("ADD"+a+b);
+		System.out.println("add"+(a+b));
+		System.out.println("sub"+(a-b));
+		System.out.println("multi"+(a*b));
+		System.out.println("div"+(a/b));
+		//perform add,sub,multi,div for long variable
+		long l=256, l1=520
+		System.out.println("ADD"+a+b);
+		System.out.println("add"+(a+b));
+		System.out.println("sub"+(a-b));
+		System.out.println("multi"+(a*b));
+		System.out.println("div"+(a/b));
+		
+	}
+}
